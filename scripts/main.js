13bd467f7a04aea4cc663a7f30ebd6c0eada69c7
glennysnaomitaiyiling-nm2207 <125462171+glennysnaomitaiyiling-nm2207@users.noreply.github.com>
2023-04-18 00:09:51 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -90,53 +90,6 @@ new Chart("income-change",
     }  
   });
 
-/* //use buttons to switch between charts for race, education and languages spoken
-let raceButton = document.getElementById("race");
-let educationButton = document.getElementById("education");
-let languagesButton = document.getElementById("languages");
-// functions to that change the content of the html page, not executed yet; only executed when eventlistener detects event
-let educationChart = function(){
-  document.getElementById("test").innerHTML = "graph for income by education level";
-  console.log("change to education chart");
-}
-
-let languagesChart = function(){
-  document.getElementById("test").innerHTML = "graph for income by language spoken";
-  console.log("change to language chart");
-}
-//event listeners on the buttons that execute the functions when a click is detected
-educationButton.addEventListener("click", educationChart);
-languagesButton.addEventListener("click", languagesChart);
- */
-//chart for expenditure vs income group
-/* const labelIncomeGroup = ["Below 1,000", //labels used for horizontal axis
-"1,000 - 1,999",
-"2,000 - 2,999",
-"3,000 - 3,999",
-"4,000 - 4,999",
-"5,000 - 5,999",
-"6,000 - 7,999",
-"8,000 - 9,999",
-"10,000 - 11,999",
-"12,000 - 14,999",
-"15,000 - 19,999",
-"20,000 & Over"]; */
-
-/* const AverageExpenditure = [
-  1929,	1958,	2047,	2537,	2904, 3257,	3851,	4535,	5115,	5809,	6832,	10047
-]; */
-
-/* const dataObj = {
-  labels: labelIncomeGroup,
-  datasets: [
-    {   label: "Average Expenditure",
-        data: AverageExpenditure,
-        borderWidth: 2,
-        backgroundColor: "hsla(49,98%,72%,0.3)",
-        borderColor: "hsla(30,100%,50%,1)"
-    },
-  ]
-} */
 
 
 new Chart("avgExpenditure-incomegroup-barchart",
